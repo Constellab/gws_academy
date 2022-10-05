7eea26eec539295238575a49c9bbd0f5bd0af42d
Benjamin <bmaisonneuve@gencovery.com>
2022-10-05 11:00:28 +0200

    create templates  for table_factor tutorial

--- tests/result/test_table_factor.py
+++ tests/result/test_table_factor.py
@@ -1,27 +1,28 @@
 
-from gws_academy import AcademyTemplateTask
+from gws_academy import OneWayAnova
 from gws_core import BaseTestCase, Table, TaskRunner
 from pandas import DataFrame
 
 
-class TemplateTest(BaseTestCase):
+class TestTableFactor(BaseTestCase):
 
     async def test_process(self):
         dataframe = DataFrame({'A': [0, 1, 2],'B': [9, 7, 5]})
-             'col3': [1, 0, 2, 4],
-             'col4': [0.1, 0.9, 6.2, 11.9],
-             'col5': [-0.2, 1.1, 5.9, 12.3]})
         table = Table(dataframe)
 
         # run trainer
         tester = TaskRunner(
-            params={'param1': 'blabla', 'param2': 1},
+            params={},
             inputs={'table': table},
-            task_type=AcademyTemplateTask
+            task_type=OneWayAnova
         )
         outputs = await tester.run()
         result = outputs['result']
 
-        # ...
-        # ... Test the result here
-        # ...
+        table = result.get_stats_table()
+        print(table)
+
+        data: DataFrame = table.get_data()
+
+        self.assertAlmostEqual(data.iat[0, 0], 0.868247, 3)  # compare with 3 digits
+        self.assertAlmostEqual(data.iat[0, 1], 0.50545, 3)
