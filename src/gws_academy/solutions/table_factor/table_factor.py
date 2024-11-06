7cb3d9d743df80a4cefbbee2dc8855a440984418
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2024-11-06 10:11:27 +0100

    (fix) remove licence comments

--- src/gws_academy/solutions/table_factor/table_factor.py
+++ src/gws_academy/solutions/table_factor/table_factor.py
@@ -1,9 +1,3 @@
-# LICENSE
-# This software is the exclusive property of Gencovery SAS.
-# The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
-# About us: https://gencovery.com
-
-
 from gws_core import (ConfigParams, InputSpec, InputSpecs, IntParam,
                       OutputSpec, OutputSpecs, Table, Task, TaskInputs,
                       TaskOutputs, task_decorator)
